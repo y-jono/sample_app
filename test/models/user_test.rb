948b52ae166d4a1460ba48e0c2840c07969c9fc9
y_jono <pek9dkl@gmail.com>
2015-12-31 12:59:55 +0900

    Make a basic User model (including secure passwords)

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,8 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
 
   def setup
-    @user = User.new(name: "Example User", email: "user@example.com")
+    @user = User.new(name: "Example User", email: "user@example.com",
+                    password: "foobar", password_confirmation: "foobar")
   end
 
   test "shoud be valid" do
@@ -35,7 +36,7 @@ class UserTest < ActiveSupport::TestCase
                          first.last@foo.jp alice+bob@baz.cn]
     valid_addresses.each do |valid_address|
       #byebug
-      @user.email = valid_addrss
+      @user.email = valid_address
       assert @user.valid?, "#{valid_address.inspect} should be valid"
     end
   end
@@ -47,6 +48,23 @@ class UserTest < ActiveSupport::TestCase
       @user.email = invalid_address
       assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
     end
+  end
+
+  test "email addresses should be unique" do
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+
+  test "password should be present (nonblank)" do
+    @user.password = @user.password_confirmation = " " * 6
+    assert_not @user.valid?
+  end
+
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
   end
 
 end
